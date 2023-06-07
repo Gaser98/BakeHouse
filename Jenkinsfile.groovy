e1785181430021558d2cb579997235a1b869ceb0
Gaser Moussa <76227165+Gaser98@users.noreply.github.com>
2023-06-08 01:32:55 +0300

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                echo 'build'
+                script{
+                    if (BRANCH_NAME == "dev" || BRANCH_NAME == "test" || BRANCH_NAME == "prod") {
+                        withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                            sh '''
+                                docker login -u ${USERNAME} -p ${PASSWORD}
+                                docker build -t gaser98/bakehouseiti:v${BUILD_NUMBER} .
+                                docker push gaser98/bakehouseiti:v${BUILD_NUMBER}
+                                echo ${BUILD_NUMBER} > ../build.txt
+                            '''
+                        }
+                    }
+                    else {
+                        echo "user choosed ${BRANCH_NAME}"
+                    }
+                }
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo 'deploy'
+                script {
+                    if (BRANCH_NAME == "release") {
+                        withCredentials([usernamePassword(credentialsId: 'kubeconfig', usernameVariable: 'USERNAME', passwordVariable:'PASSWORD')]) {
+                            sh '''
+                                export BUILD_NUMBER=$(cat ../build.txt)
+                                mv Deployment/deploy.yaml Deployment/deploy.yaml.tmp
+                                cat Deployment/deploy.yaml.tmp | envsubst > Deployment/deploy.yaml
+                                rm -f Deployment/deploy.yaml.tmp
+                                kubectl apply -f Deployment --kubeconfig ${KUBECONFIG} -n ${BRANCH_NAME}
+                            '''
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
